d4153e09a64da8cafbeb11db8a885929784369b1
20it2324-source <20it2324@rkgit.edu.in>
2026-07-29 19:39:15 +0530

    Added Second Largest - Uncategorized - Unknown

---
+++ DSA/Uncategorized/Unknown/Second_Largest/solution.py
@@ -0,0 +1,58 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        //method-1
+        /*int len=arr.length;
+        int fl=0;
+        for(int i:arr)
+        {
+            if(i>fl)
+            {
+                fl=i;
+            }
+        }
+        int ans=0;
+        int sl=0;
+        for(int i:arr)
+        {
+            if(i<fl && i>ans)
+            {
+                sl=i;
+                ans=sl;
+            }
+        }
+        if(sl!=0)
+        {
+            return sl;
+        }
+        else
+        {
+            return -1;
+        }*/
+        //method-2
+        int len=arr.length;
+        int fl=0;
+        int sl=0;
+        for(int i:arr)
+        {
+            if(i>fl)
+            {
+
+                sl=fl;
+                fl=i;
+            }
+            else if(i<fl && i>sl)
+            {
+                sl=i;
+            }
+        }
+        if(sl!=0)
+        {
+            return sl;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+}
