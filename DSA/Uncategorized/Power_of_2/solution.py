b6596f82e9326e7189ae4c92e904d135ce66f605
20it2324-source <20it2324@rkgit.edu.in>
2026-08-17 11:35:58 +0530

    Added Power of 2 - Uncategorized - Easy

--- DSA/Uncategorized/Power_of_2/solution.py
+++ DSA/Uncategorized/Power_of_2/solution.py
@@ -2,7 +2,7 @@ class Solution:
     def isPowerofTwo(self, n):
         # code here
     
-        if n & (n-1) == 0:
+        if n> 0 and (n & (n-1)) == 0:
             return True
         else:
             return False
